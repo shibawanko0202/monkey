a79b0613dacf62b2ef08b1f9c2f7c6ece05b420a
shibawanko0202 <97938730+shibawanko0202@users.noreply.github.com>
2022-03-30 17:32:45 +0900

    惜しい文字を色付け

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const start = document.getElementById("start");
 const monkey = document.getElementById("monkey");
 const text = document.getElementById("text");
+const elapsed_time = document.getElementById("elapsed_time");
 
 // const letters = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -10,38 +11,60 @@ const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 
 let play = false;
 let time;
+let start_time;
 
+//ランダム打ち込み
 function render_random(){
+  let moji = text.value;
+  let arr = [...moji];
   let ran = Math.floor(Math.random() * 26);
-  // console.log(ran)
-  monkey.textContent += letters[ran];
+  //惜しい文字を<span>で囲んで色付け
+  if(arr.indexOf(letters[ran]) > -1){
+    monkey.innerHTML += `<span>${letters[ran]}</span>`;
+  } else {
+    monkey.innerHTML += letters[ran];
+  }
   kensaku();
 };
 
+//文字の完成の見極め
 function kensaku(){
-  if(monkey.textContent.lastIndexOf(text.value) > -1){
+  // if(monkey.textContent.lastIndexOf(text.value) > -1){
+  if(monkey.textContent.indexOf(text.value,(monkey.textContent.length - text.value.length)) > -1){
     stopped();
+    let finish_time = Date.now();
+    elapsed_time.textContent = `${(finish_time - start_time) / 1000}秒 : ${monkey.textContent.length}文字で出来ました！`;
     monkey.classList.add("success");
   };
 };
 
+//停止
 function stopped(){
   play = false;
   clearInterval(time);
-}
+};
 
+//入力していないとスタート出来ないように
+text.addEventListener("input",()=>{
+  if(text.value.length > 0){
+    start.classList.remove("disabled");
+    start.textContent = "猿にお願いする";
+  } else {
+    start.classList.add("disabled");
+    start.textContent = "入力してください";
+  };
+});
 
+//スタートボタン
 start.addEventListener("click",()=>{
   if(!play){
     play = true;
-    time = setInterval(render_random,100);
-    start.textContent = "猿を止める"
-    console.log(text.value);
+    time = setInterval(render_random,10);
+    start_time = Date.now();
+    start.textContent = "猿を止める";
+    text.disabled = true;
   } else {
     stopped();
-    start.textContent = "猿に再開させる"
-  }
+    start.textContent = "猿に再開させる";
+  };
 });
-
-
-console.log(letters.length)
